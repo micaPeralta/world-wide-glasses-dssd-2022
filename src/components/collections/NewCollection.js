3c31d4aa25b81d671c39837e0e496b90cf622da8
Micaela Peralta <mika_peralta93@yahoo.com>
2022-10-06 16:00:30 -0300

    add models to New collection form

--- src/components/collections/NewCollection.js
+++ src/components/collections/NewCollection.js
@@ -1,15 +1,19 @@
 import Input from "../UI/Input";
-import {useRef} from "react";
+import {useRef, useState} from "react";
 import {NavLink, useNavigate} from "react-router-dom";
 
 const NewCollection = () => {
     const navigate = useNavigate();
+    const [models,setModels] = useState([]);
+
 
     const nameRef = useRef("")
     const descriptionRef = useRef("")
     const imagesRef = useRef("")
     const releaseDateRef = useRef(undefined)
     const manufacturingTimeRef = useRef(undefined)
+    const modelNameRef = useRef(undefined)
+    const modelTypeTimeRef = useRef(undefined)
     const modelRef = useRef()
 
     const handleCreateCollection = async (e) => {
@@ -19,7 +23,7 @@ const NewCollection = () => {
             description: descriptionRef.current.value,
             images: imagesRef.current.value,
             releaseDate: new Date(releaseDateRef.current.value).toISOString(),
-            models: [],
+            models: models,
             manufacturingTime: manufacturingTimeRef.current.value
         }
 
@@ -42,11 +46,31 @@ const NewCollection = () => {
 
     }
 
+    const handleAddModel = (e) => {
+        e.preventDefault()
+        let model = {
+            name: modelNameRef.current.value,
+            modelType: modelTypeTimeRef.current.value,
+            description: "a descriptions"
+        }
+
+        modelNameRef.current.value = "";
+        setModels([...models,model])
+    }
+
     return (
         <>
+            <nav  aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><NavLink to={"/home/collections"}> Collections </NavLink>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">New Collection</li>
+                </ol>
+            </nav>
+
             <h1 className="h3 mb-2 text-gray-800">Schedule a new collection</h1>
 
-            <NavLink to={"/home/collections"}> {"<back"}</NavLink>
+
             <div className="col-sm-6 pt-2">
                 <form onSubmit={handleCreateCollection}>
                     <div className="form-group">
@@ -65,6 +89,41 @@ const NewCollection = () => {
                         <label htmlFor="releaseDate"> Description</label>
                         <textarea ref={descriptionRef} className="form-control" id="description" rows="3" required></textarea>
                     </div>
+
+
+                    <div className="card">
+                        <div className="card-body">
+                            {models.length > 0 &&
+                                <div className="alert alert-warning" role="alert">
+                                {models.map(m =>{
+                                    return <span className="badge bg-secondary m-1 p-1">{m.name }({m.modelType})</span>
+                                })}
+                                </div>}
+                            <div className="form-group">
+                                <label htmlFor="manufacturingTime"> Model Name </label>
+                                <Input ref={modelNameRef}
+                                       name={"modelName"}
+                                       type="text"
+                                       label={"modelName"}
+                                       id="modelName"
+                                       class={"form-control form-control-user"}
+                                       required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="model">Model Type</label>
+                                <select className="form-select" id="model" ref={modelTypeTimeRef} defaultValue={"Reading lens"} >
+                                    <option value="sunglasses">Sunglasses</option>
+                                    <option value="Reading lens">Reading lens </option>
+                                </select>
+                            </div>
+
+                            <button className={"btn btn-secondary float-right"} onClick={handleAddModel}>
+                                Add
+                            </button>
+                        </div>
+                    </div>
+
 
                     <div className="form-group">
                         <label htmlFor="releaseDate"> Release Date</label>
@@ -90,13 +149,6 @@ const NewCollection = () => {
                         />
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="model">Model</label>
-                        <select className="form-select" id="model" >
-                            <option selected value="sunglasses">Sunglasses</option>
-                            <option value="Reading lens">Reading lens </option>
-                        </select>
-                    </div>
 
                     {/*<div className="form-group">*/}
                     {/*    <label htmlFor="images">Collection images:</label>*/}
@@ -111,9 +163,11 @@ const NewCollection = () => {
                     {/*    />*/}
                     {/*</div>*/}
 
-                    <button className={"btn btn-secondary float-right"} type={"submit"}>
+                    <div className="d-grid gap-2">
+                    <button className={"btn btn-secondary btn- "} type={"submit"}>
                         Submit
                     </button>
+                    </div>
                 </form>
 
             </div>
